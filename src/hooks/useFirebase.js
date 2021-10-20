be3dedc36370bbf942390e699c00e90fab1a2037
Mrhrab <meharabhossain5@gmail.com>
2021-10-20 15:13:09 +0200

    login register section done

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider , signOut, onAuthStateChanged} from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider , signOut, onAuthStateChanged,createUserWithEmailAndPassword,signInWithEmailAndPassword  } from "firebase/auth";
 import { useEffect, useState } from "react";
 import initializeAuthentication from "../Firebase/firebase.init";
 
@@ -6,14 +6,19 @@ import initializeAuthentication from "../Firebase/firebase.init";
 initializeAuthentication();
 
 const useFirebase = () => {
+    const [email, setEmail] =useState('');
+    const [password, setPassword] =useState('');
+    const [error, setError]= useState('');
     const [user, setUser] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const [isLogin, setIsLogin] =useState(false);
 
     const auth = getAuth();
 
     const signInUsingGoogle = () => {
         setIsLoading(true)
         const googleProvider = new GoogleAuthProvider();
+
 
         signInWithPopup(auth, googleProvider)
         .then(result => {
@@ -22,6 +27,56 @@ const useFirebase = () => {
         .finally(()=> setIsLoading(false));
 
 
+    } 
+
+
+    const toggleLogin = e => {
+        setIsLogin(e.target.checked)
+    }
+
+    const handleEmailChange = e => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value);
+    }
+
+    const handleRegistration = e => {
+        e.preventDefault();
+        console.log(email, password);
+        if(password.length < 6){
+            setError('Password Must be at least 6 characters long')
+            return;
+        }
+        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+            setError('Password must contain 2 upper case')
+        }
+       
+        isLogin? processlogin(email, password) : registerNewUser(email, password);
+    }
+    const processlogin = (email, password) =>{
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            setError('')
+        })
+        .catch(error => {
+            setError(error.message);
+        })
+    }
+
+    const registerNewUser =(email, password) => {
+         createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const user= result.user;
+            console.log(user);
+            setError('');
+        })
+        .catch(error => {
+            setError(error.message);
+        })
     }
 
     // observe state change
@@ -50,7 +105,15 @@ const useFirebase = () => {
     return {
         user,
         signInUsingGoogle,
-        logOut
+        logOut,
+
+        handleEmailChange,
+        handlePasswordChange,
+        handleRegistration,
+        toggleLogin,
+        error,
+        isLogin
+
     }
 }
 
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,14 +4,24 @@ import { Container, Form } from 'react-bootstrap';
 import useAuth from '../../hooks/useAuth';
 
 const Login = () => {
-    const { signInUsingGoogle } = useAuth(); 
+    const { 
+            signInUsingGoogle,
+            handleEmailChange,
+            handlePasswordChange,
+            handleRegistration,
+            toggleLogin,
+            isLogin,
+            error
+
+                                 } = useAuth(); 
     return (
         <div id="login">
             <Container>
+                <h1>Please {isLogin ? 'Login' : 'Register'}</h1>
                 <Form className="mt-5">
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control onChange={handleEmailChange} type="email" required placeholder="Enter email" />
                         <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                         </Form.Text>
@@ -19,22 +29,26 @@ const Login = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control onChange={handlePasswordChange} type="password" required placeholder="Password" />
                     </Form.Group>
+                    <p className="text-danger">{error}</p>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+
+                        <Form.Check onChange={toggleLogin} type="checkbox" label="Already Registered?" />
+
                     </Form.Group>
-                    <Button variant="primary" type="submit" className="my-2">
-                        Register
+                    <Button onClick={handleRegistration} variant="primary" type="submit" className="my-2">
+                        {isLogin ? 'Login' : 'Register'}
                     </Button>
                     <br />
                     
                     <br />
                     <p>OR</p>
-                    <Button onClick={signInUsingGoogle} variant="primary" type="submit" className="">
-                        Login With Google
-                    </Button>
+                    
                 </Form>
+                <Button onClick={signInUsingGoogle} variant="primary" type="submit" className="">
+                        Continue With Google
+                    </Button>
             </Container>
         </div>
     );
